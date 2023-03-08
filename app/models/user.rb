d39e272348e52bb5706070b1070fc0a5b5b008fa
Guilherme Mello <guipcmello@gmail.com>
2023-03-08 17:04:50 -0300

    added devise functions & updated registration forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, :password, presence: true
+  validates :username, :email, presence: true
   validates :email, :username, uniqueness: true
 end
