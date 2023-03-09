f270eccc3eaabbb2dbc7ab6e7cc0ecb55cb89557
Fabio Waideman <fawaideman@yahoo.com.br>
2023-03-09 15:42:08 -0300

    added search engine and books index route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+
+  def index
+    @books = Book.all
+    authorize @books
+    if params[:query].present?
+      @books = @books.where("title ILIKE ?", "%#{params[:query]}%")
+    end
+  end
 end
