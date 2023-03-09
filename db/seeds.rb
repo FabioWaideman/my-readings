f2eace7ff71d5af705d52aac365f0d0271b89ffe
Fabio Waideman <fawaideman@yahoo.com.br>
2023-03-09 14:01:45 -0300

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'net/http'
+require 'json'
+
+key = ENV["BOOK_API_KEY"]
+
+categories = ["fantasy",
+              "children",
+              "adventure",
+              "science fiction",
+              "art", "thriller",
+              "suspense",
+              "horror",
+              "mystery",
+              "romance"]
+
+Book.destroy_all
+
+categories.each do |c|
+  url = "https://www.googleapis.com/books/v1/volumes?q=+subject:#{c}&maxResults=40&key=#{key}"
+  fetch = Net::HTTP.get(URI(url))
+  parse = JSON.parse(fetch)
+  response = parse['items']
+  if response.nil?
+    puts "No results found for category #{c}"
+  else
+    response.each do |element|
+      book = Book.new
+      book.title = element["volumeInfo"]["title"]
+      # element["volumeInfo"]["subtitle"].nil? ? book.subtitle = "" : book.subtitle = element["volumeInfo"]["imageLinks"]["smallThumbnail"]
+      book.author = element["volumeInfo"]["authors"].nil? ? "Author not found" : element["volumeInfo"]["authors"].first
+      book.average_rating = element["volumeInfo"]["averageRating"].nil? ? 0 : element["volumeInfo"]["averageRating"]
+      element["volumeInfo"]["imageLinks"].nil? ? book.image_url = ActionController::Base.helpers.asset_path("dummybook.png") : book.image_url = element["volumeInfo"]["imageLinks"]["smallThumbnail"]
+      book.page_count = element["volumeInfo"]["pageCount"]
+      book.description = element["volumeInfo"]["description"]
+      book.categories = element["volumeInfo"]["categories"]
+      book.published_date = element["volumeInfo"]["publishedDate"]
+      book.save!
+    end
+  end
+end
