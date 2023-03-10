0d2a5ff34c4d4b87a0ffaa9a20c51f1166d250ac
Fabio Waideman <fawaideman@yahoo.com.br>
2023-03-10 15:50:50 -0300

    fixed coflicts

--- app/policies/book_policy.rb
+++ app/policies/book_policy.rb
@@ -1,16 +1,4 @@
 class BookPolicy < ApplicationPolicy
-<<<<<<< HEAD
-  def show?
-    true
-  end
-
-  def index?
-    true
-  end
-
-=======
-  
->>>>>>> master
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
 
