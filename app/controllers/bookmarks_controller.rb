38bacc2361bbbb39e3b2ec124aa6d553e3da0ae0
Fabio Waideman <fawaideman@yahoo.com.br>
2023-03-10 17:55:06 -0300

    commit for merge

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,7 @@
 class BookmarksController < ApplicationController
+  def create
+    @book = Book.find(params[:id])
+    @bookmark = Bookmark.new(user: current_user, book: @book)
+    @bookmark.save
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+  has_many :users, through: :bookmarks
 end
