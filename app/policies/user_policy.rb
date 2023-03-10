a2223f27b6f33f81a1d354e55e9f299b0fd76ae4
Fabio Waideman <fawaideman@yahoo.com.br>
2023-03-10 15:23:24 -0300

    added user profile

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,8 +1,6 @@
-class BookPolicy < ApplicationPolicy
-  
+class UserPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-
     def resolve
       scope.all
     end
