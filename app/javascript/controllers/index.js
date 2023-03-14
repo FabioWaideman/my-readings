4d440694bd0053fc3463e1c12f8e58c614baf659
Guilherme Mello <guipcmello@gmail.com>
2023-03-14 12:07:46 -0300

    added reviews & reviews filter & reviews section layout

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import ControllerReviewController from "./controller_review_controller"
+application.register("controller-review", ControllerReviewController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
