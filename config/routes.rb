9d10334db93387f82c5c56252f853e3072b55df5
Lenilda Miranda <lennymir24@gmail.com>
2023-03-15 21:04:44 -0300

    Sign Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   root to: "pages#home"
+  get '/sign_in', to: 'sessions#new'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
